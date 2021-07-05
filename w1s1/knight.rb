9688443fcc62cbdb790094020917c6afcac3f88d
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-07-06 06:18:04 +0700

    Utilize new attribute instead of inheritance to implement dodge attack

--- w1s1/knight.rb
+++ w1s1/knight.rb
@@ -1,21 +1,31 @@
 class Knight
 
-    def initialize(name, hitpoint, attack_damage)
+    def initialize(name, hitpoint, attack_damage, agility=0)
         @name = name
         @hitpoint = hitpoint
         @attack_damage = attack_damage
+        @agility = agility
 
         puts "#{self}\n\n"
     end
 
     def attack(other_knight)
-        puts "#{@name} attacks #{other_knight.name} with #{@attack_damage} damage"
+        puts("#{@name} attacks #{other_knight.name} with #{@attack_damage} damage")
         other_knight.damaged(@attack_damage)
     end
 
     def damaged(damage)
-        @hitpoint -= damage
-        puts self
+        if rand(100) < @agility
+            dodge_attack()
+        else
+            @hitpoint -= damage
+        end
+
+        puts(self)
+    end
+
+    def dodge_attack()
+        puts("#{@name} deflects the attack")
     end
 
     def to_s()
--- w1s1/main.rb
+++ w1s1/main.rb
@@ -1,8 +1,7 @@
-require_relative "agile_knight.rb"
 require_relative "knight.rb"
 require_relative "turn_based_combat.rb"
 
-knight1 = AgileKnight.new("Jin Sakai", 100, 50)
+knight1 = Knight.new("Jin Sakai", 100, 50, 80)
 knight2 = Knight.new("Khotun Khan", 500, 50)
 
 turn_based_combat = TurnBasedCombat.new(knight1, knight2)
