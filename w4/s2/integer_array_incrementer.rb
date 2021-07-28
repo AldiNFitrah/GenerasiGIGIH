c06e42640d0466caf086cbfb631855d21078b1fa
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-07-28 19:16:28 +0700

    test(integer-array-incrementer): pass fifth test

--- w4/s2/integer_array_incrementer.rb
+++ w4/s2/integer_array_incrementer.rb
@@ -3,7 +3,11 @@ class IntegerArrayIncrementer
     new_arr = arr[0..-2] + [arr[-1] + 1]
 
     if new_arr[-1] == 10 then
-      return [1, 0]
+      if new_arr.length == 1 then
+        return [1, 0]
+      else
+        return [new_arr[0] + 1, 0]
+      end
     end
 
     return new_arr
