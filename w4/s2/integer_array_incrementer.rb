93c02cc1247c28081f400d6651de8511a7b95cf3
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-07-28 19:40:31 +0700

    test(integer-array-increment): pass test for 2 digit with 9 at the end

--- w4/s2/integer_array_incrementer.rb
+++ w4/s2/integer_array_incrementer.rb
@@ -5,8 +5,12 @@ class IntegerArrayIncrementer
     if new_arr[-1] == 10 then
       if new_arr.length == 1 then
         return [1, 0]
-      else
-        return [new_arr[0] + 1, 0]
+      elsif new_arr.length == 2 then
+        if new_arr[0] == 9
+          return [1, 0, 0]
+        else
+          return [new_arr[0] + 1, 0]
+        end
       end
     end
 
--- w4/s2/spec/integer_array_incrementer_rspec.rb
+++ w4/s2/spec/integer_array_incrementer_rspec.rb
@@ -22,54 +22,36 @@ describe IntegerArrayIncrementer do
   it 'given [9] should return [1, 0]' do
     input = [9]
     expected_output = [1, 0]
-  
+
     actual_output = IntegerArrayIncrementer.new.increment(input)
-  
+
     expect(actual_output).to(eq(expected_output))
   end
 
   it 'given [1, 0] should return [1, 1]' do
     input = [1, 0]
     expected_output = [1, 1]
-  
+
     actual_output = IntegerArrayIncrementer.new.increment(input)
-  
+
     expect(actual_output).to(eq(expected_output))
   end
 
   it 'given [1, 9] should return [2, 0]' do
     input = [1, 9]
     expected_output = [2, 0]
-  
-    actual_output = IntegerArrayIncrementer.new.increment(input)
-  
-    expect(actual_output).to(eq(expected_output))
-  end
 
-  it 'given [5, 9] should return [6, 0]' do
-    input = [5, 9]
-    expected_output = [6, 0]
-  
     actual_output = IntegerArrayIncrementer.new.increment(input)
-  
-    expect(actual_output).to(eq(expected_output))
-  end
 
-  it 'given [5, 9] should return [6, 0]' do
-    input = [5, 9]
-    expected_output = [6, 0]
-  
-    actual_output = IntegerArrayIncrementer.new.increment(input)
-  
     expect(actual_output).to(eq(expected_output))
   end
 
   it 'given [9, 9] should return [1, 0, 0]' do
     input = [9, 9]
     expected_output = [1, 0, 0]
-  
+
     actual_output = IntegerArrayIncrementer.new.increment(input)
-  
+
     expect(actual_output).to(eq(expected_output))
   end
 end
