ad6017ca03f03805e98c19f1798805ae580105d4
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-07-31 22:23:17 +0700

    test(category-controller): create tests for show_all method

--- w4/s3/spec/controllers/category_controller_spec.rb
+++ w4/s3/spec/controllers/category_controller_spec.rb
@@ -83,4 +83,39 @@ describe CategoryController do
       end
     end
   end
+
+  describe '.show_all' do
+    context 'given 2 categories' do
+      before(:each) do
+        @category1 = Category.new({
+          name: "cat1",
+        })
+        @category2 = Category.new({
+          name: "cat2",
+        })
+
+        @category1.save()
+        @category2.save()
+      end
+
+      it 'should return 2 categories inside hash' do
+        expected_return_value = {categories: [@category1, @category2]}
+        actual_return_value = controller.show_all()
+
+        expect(actual_return_value).to(eq(expected_return_value))
+      end
+    end
+
+    context 'given 0 categories' do
+      it 'should return 0 categories inside hash' do
+        expected_return_value = {categories: []}
+        actual_return_value = controller.show_all()
+
+        expect(actual_return_value).to(eq(expected_return_value))
+      end
+    end
+  end
+
+
+
 end
