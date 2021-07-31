c56d98b7f4588783559c8d3e027c44d474b6ba93
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-07-31 23:14:20 +0700

    refactor(main-app): use modular SinatraApp approach

--- w4/s3/main.rb
+++ w4/s3/main.rb
@@ -8,135 +8,142 @@ require './models/category.rb'
 require './controllers/item_controller.rb'
 require './controllers/category_controller.rb'
 
-get '/' do
-  return erb(
-    :index,
-    layout: :base
-  )
-end
+class FoodOmsApp < Sinatra::Base
 
-namespace '/category' do
-  get '' do
-    data = CategoryController.show_all()
+  register Sinatra::Namespace
+
+  get '/' do
     return erb(
-      :"category/list",
-      layout: :base,
-      locals: data,
+      :index,
+      layout: :base
     )
   end
 
-  get '/new' do
-    return erb(
-      :"category/new",
-      layout: :base,
-    )
-  end
+  namespace '/category' do
+    get '' do
+      data = CategoryController.show_all()
+      return erb(
+        :"category/list",
+        layout: :base,
+        locals: data,
+      )
+    end
 
-  post '/create' do
-    CategoryController.create(params)
+    get '/new' do
+      return erb(
+        :"category/new",
+        layout: :base,
+      )
+    end
 
-    return redirect("/category")
-  end
-  
-  get '/:id/delete' do
-    CategoryController.delete(params)
+    post '/create' do
+      CategoryController.create(params)
 
-    return redirect("/category")
-  end
-
-  get '/:id/edit' do
-    data = CategoryController.details(params)
-    return erb(
-      :"category/edit",
-      layout: :base,
-      locals: data,
-    )
-  end
-
-  post '/:id/update' do
-    data = CategoryController.update(params)
-    return redirect("/category")
-  end
-
-  get '/:id/item/:item_id/remove' do
-    CategoryController.remove_item(params)
-    return redirect("/category/#{params["id"]}/edit")
-  end
-
-  get '/:id' do
-    data = CategoryController.details(params)
-    return erb(
-      :"category/details",
-      layout: :base,
-      locals: data
-    )
-  end
-end
-
-namespace '/item' do
-  get '' do
-    data = ItemController.show_all()
-    return erb(
-      :"item/list",
-      layout: :base,
-      locals: data,
-    )
-  end
-
-  get '/new' do
-    data = ItemController.new_form()
+      return redirect("/category")
+    end
     
+    get '/:id/delete' do
+      CategoryController.delete(params)
+
+      return redirect("/category")
+    end
+
+    get '/:id/edit' do
+      data = CategoryController.details(params)
+      return erb(
+        :"category/edit",
+        layout: :base,
+        locals: data,
+      )
+    end
+
+    post '/:id/update' do
+      data = CategoryController.update(params)
+      return redirect("/category")
+    end
+
+    get '/:id/item/:item_id/remove' do
+      CategoryController.remove_item(params)
+      return redirect("/category/#{params["id"]}/edit")
+    end
+
+    get '/:id' do
+      data = CategoryController.details(params)
+      return erb(
+        :"category/details",
+        layout: :base,
+        locals: data
+      )
+    end
+  end
+
+  namespace '/item' do
+    get '' do
+      data = ItemController.show_all()
+      return erb(
+        :"item/list",
+        layout: :base,
+        locals: data,
+      )
+    end
+
+    get '/new' do
+      data = ItemController.new_form()
+      
+      return erb(
+        :"item/new",
+        layout: :base,
+        locals: data,
+      )
+    end
+
+    post '/create' do
+      ItemController.create(params)
+
+      return redirect("/item")
+    end
+
+    get '/:id/delete' do
+      ItemController.delete(params)
+      
+      return redirect("/item")
+    end
+
+    get '/:id/edit' do
+      data = ItemController.edit(params)
+      return erb(
+        :"item/edit",
+        layout: :base,
+        locals: data,
+      )
+    end
+
+    post '/:id/update' do
+      data = ItemController.update(params)
+      return redirect("/item")
+    end
+
+    get '/:id/category/:category_id/remove' do
+      ItemController.remove_category(params)
+      return redirect("/item/#{params['id']}/edit")
+    end
+
+    get '/:id' do
+      data = ItemController.details(params)
+      return erb(
+        :"item/details",
+        layout: :base,
+        locals: data
+      )
+    end
+  end
+
+  not_found do
     return erb(
-      :"item/new",
+      :page404,
       layout: :base,
-      locals: data,
     )
   end
 
-  post '/create' do
-    ItemController.create(params)
-
-    return redirect("/item")
-  end
-
-  get '/:id/delete' do
-    ItemController.delete(params)
-    
-    return redirect("/item")
-  end
-
-  get '/:id/edit' do
-    data = ItemController.edit(params)
-    return erb(
-      :"item/edit",
-      layout: :base,
-      locals: data,
-    )
-  end
-
-  post '/:id/update' do
-    data = ItemController.update(params)
-    return redirect("/item")
-  end
-
-  get '/:id/category/:category_id/remove' do
-    ItemController.remove_category(params)
-    return redirect("/item/#{params['id']}/edit")
-  end
-
-  get '/:id' do
-    data = ItemController.details(params)
-    return erb(
-      :"item/details",
-      layout: :base,
-      locals: data
-    )
-  end
-end
-
-not_found do
-  return erb(
-    :page404,
-    layout: :base,
-  )
+  run!
 end
